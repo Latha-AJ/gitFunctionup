4112993424df279c9d782d29613b4b146802acd7
Latha-AJ <lathajappa.7226@gmail.com>
2022-06-02 20:04:56 +0530

    this is the modified git intro file

---
+++ src/routes/logger.js
@@ -0,0 +1,8 @@
+const endpoint = "https://www.functionup.org"
+const  log = function(){
+    console.log('I am inside the log function')
+}
+
+
+module.exports.url = endpoint;
+module.exports.myfunction = log;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const logger = require('./logger');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/test-me1', function (req, res) {
     res.send('My first ever api!')
+});
+
+router.get('/test-me2', function (req, res) {
+  console.log('The constant in logger route has a value' + logger.url)  
+  logger.myfunction();
+    res.send('My second  ever api!')
+});
+
+router.get('/test-me3', function (req, res) {
+    res.send('My third ever api!')
+});
+
+router.get('/test-me4', function (req, res) {
+    res.send('My fourth ever api!')
 });
 
 module.exports = router;
