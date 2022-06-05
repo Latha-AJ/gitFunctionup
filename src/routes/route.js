7d03b1e0f8a8408cc767e517d4791ac4502e5f5a
Latha-AJ <lathajappa.7226@gmail.com>
2022-06-05 10:00:08 +0530

    I am commiting the changes to the missing number assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -21,5 +21,32 @@ router.get('/test-me4', function (req, res) {
     res.send('My fourth ever api!')
 });
 
+router.get('/hi', function (req, res) {
+     let arr = [1,2,3,5];
+     let total=0;
+     for (let n=0 ; n<4 ; n++)
+     {
+       total += arr[n];
+     }
+    const lastEle = arr.pop();
+    let sum = lastEle*(lastEle+1)/2
+    const missingNumber = sum-total
+    res.send({"missingNumber" : missingNumber});
+});
+
+router.get('/soln', function (req, res) {
+    let array = [33,34,35,37,38]
+    let total=0;
+    let len=array.length;
+    for(var i in array)
+    {
+        total +=array[i]
+    }
+    let firstEle=array[0];
+    let lastEle=array.pop();
+    let consecutiveSum = (len+1)*(firstEle+lastEle)/2;
+    let missingNumber = consecutiveSum-total;
+    res.send({"missingNumber": missingNumber})
+ });
 module.exports = router;
 // adding this comment for no reason
