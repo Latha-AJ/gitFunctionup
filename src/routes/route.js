59cf309218285002dc10178c4bc1f7fafeadccc2
Latha-AJ <lathajappa.7226@gmail.com>
2022-06-06 09:06:22 +0530

    Adding players assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,28 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+     let players = [];
+
+router.post("/players", function(req, res) {
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+        for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+     if (isNameRepeated) {
+       res.send("This player was already added!")
+    } else {
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
+    
+
 module.exports = router;
